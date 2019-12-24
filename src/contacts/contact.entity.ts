80a01241dacd4dc8eec44e0ddab0ae0f11375904
hugohoffmann <hugohoffmann04@gmail.com>
2019-12-23 23:25:33 -0300

    new: TypeORM entity Model

---
+++ src/contacts/contact.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Contact {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    country: string;
+
+}
--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts/contacts.service';
 import { ContactsController } from './contacts/contacts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './contact.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Contact]),
+  ],
   providers: [ContactsService],
   controllers: [ContactsController]
 })
