67ec234dac540b7ce1c6d39ae8b4f8507676c1e9
hugohoffmann <hugohoffmann04@gmail.com>
2019-12-23 23:32:29 -0300

    new: CRUD service Contact

--- src/contacts/contacts/contacts.service.ts
+++ src/contacts/contacts/contacts.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Repository, DeleteResult, UpdateResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from '../contact.entity'
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+    constructor(
+        @InjectRepository(Contact)
+        private contactRepository: Repository<Contact>,
+    ) {}
+
+    async  findAll(): Promise<Contact[]> {
+        return await this.contactRepository.find();
+    }
+
+    async  create(contact: Contact): Promise<Contact> {
+        return await this.contactRepository.save(contact);
+    }
+
+    async update(contact: Contact): Promise<UpdateResult> {
+        return await this.contactRepository.update(contact.id, contact);
+    }
+
+    async delete(id): Promise<DeleteResult> {
+        return await this.contactRepository.delete(id);
+    }
+}
