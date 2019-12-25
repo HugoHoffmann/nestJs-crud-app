168a9ffa3641f3ffcb67a8f0ec8f72f7d2ec914c
hugohoffmann <hugohoffmann04@gmail.com>
2019-12-25 18:45:39 -0300

    feat(contact): endpoint update(put)

--- src/contacts/contacts/contacts.controller.ts
+++ src/contacts/contacts/contacts.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { Contact } from '../contact.entity';
 import { ContactsService } from '../contacts/contacts.service';
+import { identity } from 'rxjs';
 
 @Controller('contacts')
 export class ContactsController {
@@ -14,6 +15,13 @@ export class ContactsController {
     @Post('create')
     async create(@Body() contactData: Contact): Promise<any> {
         return this.contactService.create(contactData)
+    }
+
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() contactData: Contact): Promise<any> {
+        contactData.id = Number(id);
+        console.log(`Update #${contactData.id}`);
+        return this.contactService.update(contactData);
     }
    
 }
